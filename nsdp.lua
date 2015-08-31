9899c52f27fd98966ccbc91177cd6d4c948dba64
Sebastian Wagner <2000sw@gmail.com>
2015-09-01 01:36:50 +0200

    version, operation offset

--- nsdp.lua
+++ nsdp.lua
@@ -6,9 +6,10 @@ function nsdp_proto.dissector(buffer,pinfo,tree)
     pinfo.cols.protocol = "NSDP"
     local subtree = tree:add(nsdp_proto,buffer(),"Netgrear NSDP Data")
     subtree:add(buffer(0,2),"Version: " .. buffer(0,2):uint())
-    subtree = subtree:add(buffer(2,2),"Operation")
-    subtree:add(buffer(2,1),"The 3rd byte: " .. buffer(2,1):uint())
-    subtree:add(buffer(3,1),"The 4th byte: " .. buffer(3,1):uint())
+    subtree:add(buffer(2,2),"Operation: " .. buffer(2,2):uint())
+    subtree = subtree:add(buffer(4,2),"Operation")
+    subtree:add(buffer(4,1),"The 3rd byte: " .. buffer(4,1):uint())
+    subtree:add(buffer(4,1),"The 4th byte: " .. buffer(4,1):uint())
 end
 -- load the udp.port table
 udp_table = DissectorTable.get("udp.port")
